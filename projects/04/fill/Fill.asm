38b8a78010cabdd803ec0041433892acd513ab77
jburnworth <j.j.burnworth@gmail.com>
2014-11-07 12:27:56 -0700

    Wrote Fill.asm to finish Ch. 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,47 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+// Define location to keep track erase/fill
+	@SCREEN
+	D=A
+	@location
+	M=D
+// Check if location is outside of SCREEN and if a key is pressed
+(CHECK)
+	@location
+	D=M         // D = location
+	@KBD
+	D=D-A       // D = location - 24576
+	@KEYCHECK
+	D;JLT       // If location is within screen size, jump to KEYCHECK
+	@SCREEN
+	D=A
+	@location  	
+	M=D    // Else, reset location to first screen address
+(KEYCHECK)
+	@KBD
+	D=M
+	@FILL
+	D;JNE       // If KBD is not 0, begin filling screen
+	@ERASE
+	0;JMP       // If KBD is 0, begin erasing
+
+// Fill the screen
+(FILL)
+	@location
+	A=M
+	M=-1        // Screen addr. held in location = 0xFF
+	@location
+	M=M+1       // location++
+	@CHECK
+	0;JMP       // GOTO CHECK
+
+// Erase the screen
+(ERASE)
+	@location
+	A=M
+	M=0         // Screen addr. held in location = 0x00
+	@location
+	M=M+1       // location++
+	@CHECK
+	0;JMP       // GOTO CHECK
