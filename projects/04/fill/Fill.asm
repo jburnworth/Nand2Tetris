349bb2e0c71d950dfd8a9f32e0760fcf7dc13de9
James Burnworth <j.j.burnworth@gmail.com>
2019-12-22 21:18:48 -0700

    Copy in latest zip to reset my old work and update all tools.
    No changes in projects/01 because I already coppied it in.
    See last commit.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -3,52 +3,12 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, the
-// program clears the screen, i.e. writes "white" in every pixel.
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
 
-// Define location to keep track erase/fill
-	@SCREEN
-	D=A
-	@location
-	M=D
-// Check if location is outside of SCREEN and if a key is pressed
-(CHECK)
-	@location
-	D=M         // D = location
-	@KBD
-	D=D-A       // D = location - 24576
-	@KEYCHECK
-	D;JLT       // If location is within screen size, jump to KEYCHECK
-	@SCREEN
-	D=A
-	@location  	
-	M=D    // Else, reset location to first screen address
-(KEYCHECK)
-	@KBD
-	D=M
-	@FILL
-	D;JNE       // If KBD is not 0, begin filling screen
-	@ERASE
-	0;JMP       // If KBD is 0, begin erasing
-
-// Fill the screen
-(FILL)
-	@location
-	A=M
-	M=-1        // Screen addr. held in location = 0xFF
-	@location
-	M=M+1       // location++
-	@CHECK
-	0;JMP       // GOTO CHECK
-
-// Erase the screen
-(ERASE)
-	@location
-	A=M
-	M=0         // Screen addr. held in location = 0x00
-	@location
-	M=M+1       // location++
-	@CHECK
-	0;JMP       // GOTO CHECK
+// Put your code here.
