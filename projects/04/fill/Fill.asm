558f2a587a1681903b71aa0e77054bf42e5b1f6d
James Burnworth <j.j.burnworth@gmail.com>
2020-01-15 05:58:07 +0000

    Finished chapter 04
    Assembly program to multiply and fill the screen if a key is pressed.
    
    Modified gitignore to ignore *.hack

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,52 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Loop through the screens memory space
+// for (i=0; i < (256*512); i++)
+//   if key pressed
+//     fill screen
+//   else
+//     clear screen
+
+@i
+M=0	// initialize i
+
+(LOOP)
+@i
+D=M	// Hold i in D
+@8192	// (256*32)-1
+D=D-A
+@RESTART
+D;JGT	// if i < (256*32) goto RESTART and restart i
+	// using jump if i-(256*32-1) > 0
+@KBD
+D=M	// Hold kbd in D
+@CLEAR
+D;JEQ	// if kbd == 0 goto CLEAR, else set to black
+@i
+D=M	// Hold i in D
+@SCREEN
+A=D+A	// set addr to screen base plus i
+M=-1	// fill the current screen word
+@INC
+0;JMP
+
+(CLEAR)
+@i
+D=M	// Hold i in D
+@SCREEN
+A=D+A	// set addr to screen base plus i
+M=0	// clear the current screen word
+
+(INC)
+@i
+M=M+1	// i++
+@LOOP
+0;JMP	// Loop again
+
+(RESTART)
+// Set i back to zero, re-loop through memory space
+@i
+M=0
+@LOOP
+0;JMP	// Jump to Loop
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,30 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// for (i=1; i <= R1; i++)
+//   R2 += R0
+
+@R2
+M=0	// Initialize R2 to 0
+@i
+M=1	// We'll use i as the iterator in the for loop
+
+(LOOP)
+@i
+D=M	// Hold i in D
+@R1	// Load the value of R1 in M
+D=D-M	// D=i-R1
+@END
+D;JGT	// if (i-R1)>0 goto END
+@R0
+D=M	// Hold R0 in D
+@R2
+M=D+M	// R2 = R0 + R2
+@i
+M=M+1	// i++
+@LOOP
+0;JMP	// repeat
+
+(END)
+@END
+0;JMP	// End of program infinite loop
