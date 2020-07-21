3ef07b9bc18c2b74adafab110d433cb8e210822a
James Burnworth <j.j.burnworth@gmail.com>
2020-07-20 22:51:00 -0600

    Starting with Chapter 10
    Almost done with very basic implementation of class structure.
    Passing files around and what not.  But now I'm tired.

---
+++ projects/10/analyzer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3.8
+
+import tokenizer
+#import compilation
+
+import sys, os
+
+# Get input, single .jack file or directory containing .jack file(s)
+try:
+    projectInput = str(sys.argv[1])
+except:
+    print('Please pass one argument, the path to a .jack file or a directory containing .jack files')
+    sys.exit(1)
+
+# Parse project name from input
+# If a file, use file name without .jack extension,
+# if a directory, use last directory in path
+project = os.path.splitext(projectInput)[0]
+
+# Find all the files we need to analyze
+jackFileArray = []
+# Check if single file is passed
+if os.path.isfile(projectInput) and '.jack' in projectInput:
+    jackFileArray.append(projectInput)
+# Or check if a directory is passed
+elif os.path.isdir(projectInput):
+    for file in os.listdir(projectInput):
+        if '.jack' in file:
+            jackFileArray.append(projectInput + '/' + file)
+
+# Check if any .jack files were found
+if len(jackFileArray) < 1:
+    print('No .jack files found: Pass path to a .jack file or a directory containing .jack files')
+    sys.exit(1)
+
+for jackFile in jackFileArray:
+    # Tokenize the file.
+    # Compile tokens
+print(project)
+print(jackFileArray)
+print(len(jackFileArray))
---
+++ projects/10/tokenizer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.8
+
+class Tokenizer:
+    def __init__(self, jackFile):
+        # Initialize class object and open the file
+        self.jackFile = jackFile
+
+    def getTokenDict(self):
+        # Read through file, return tuple of tokens
+        true
